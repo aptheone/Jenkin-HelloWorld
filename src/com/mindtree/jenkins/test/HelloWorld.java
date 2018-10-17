a76e995c5ec21f9af6f4eeb36316b497aaf1d63c
Anand Prakash <aptheone@gmail.com>
2018-10-17 03:22:54 +0000

    first commit

---
+++ src/com/mindtree/jenkins/test/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mindtree.jenkins.test;
+
+/**
+ * @author LabAdmin
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello Jenkins !!");
+	}
+
+}
